678e8aaa54d5ba9926daa58bfc6da65cc6d937ab
NIHARIKA983 <niharikagowda1996@gmail.com>
2021-07-27 17:47:55 +0530

    Added:The Two Strings are Anagram or not

--- src/main/java/com/bridgelabz/InsertionSort.java
+++ src/main/java/com/bridgelabz/InsertionSort.java
@@ -22,7 +22,7 @@ public class InsertionSort {
         int n = arr.length;
         for(int i=0;i<n;i++)
             System.out.println(arr[i]+"");
-        System.out.println();
+
     }
     public static void main(String args[])
     {
